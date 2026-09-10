e270e1b57fd82f679c46101bb56e56b6bd9e0cfd
Milene-Arla <milene16@outlook.com>
2026-09-09 21:01:56 -0300

    Add PostgreSQL dependency to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	runtimeOnly("org.postgresql:postgresql")
 }
 
 kotlin {
